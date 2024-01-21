cf3c562e1a63459bb50bd1234d4e6ccdf93682c3
Masood Ahmed <mangimasood2000@gmail.com>
2024-01-21 13:10:00 +0800

    removed redundant folders

--- server/routes/general.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getUser } from "../controllers/general.js";
-
-const router = express.Router();
-
-router.get("/user/:id", getUser); // TODO: change to stock information
-
-export default router;
