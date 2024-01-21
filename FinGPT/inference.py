47e66333d4b1dec853bfbb5e5799df128c16c9b1
Aryan Agrawal <80872992+Codes-of-Aryan@users.noreply.github.com>
2024-01-21 12:55:43 +0800

    fingpt inference added

---
+++ FinGPT/inference.py
@@ -0,0 +1,18 @@
+from datasets import load_dataset
+from transformers import AutoTokenizer, AutoModelForCausalLM
+from peft import PeftModel
+
+
+base_model = AutoModelForCausalLM.from_pretrained(
+    'meta-llama/Llama-2-7b-chat-hf',
+    trust_remote_code=True,
+    device_map="auto",
+    token="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"
+    # torch_dtype=torch.float16,   # optional if you have enough VRAM
+)
+tokenizer = AutoTokenizer.from_pretrained(
+    'FinGPT/fingpt-forecaster_dow30_llama2-7b_lora')
+
+model = PeftModel.from_pretrained(
+    'FinGPT/fingpt-forecaster_dow30_llama2-7b_lora')
+model = model.eval()
